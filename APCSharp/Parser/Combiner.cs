d3bde8571318bcf5a96052e0a1d38468f58e1cf9
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-09-03 00:22:42 +0200

    Added Combiner Names

--- APCSharp/Parser/Combiner.cs
+++ APCSharp/Parser/Combiner.cs
@@ -30,15 +30,24 @@ namespace APCSharp.Parser
         /// Create a new combiner function assuming Nodes are Elements.
         /// </summary>
         /// <param name="func">Node combiner function</param>
+        public  string Name { get; internal set; }
         public Combiner(Func<Node<TNode>, Node<TNode>, Node<TNode>> func) : this(CombinerType.Elements, func) { }
         /// <summary>
         /// Create a new combiner.
         /// </summary>
         /// <param name="type">Compatible Node types</param>
         /// <param name="func">Node combiner function</param>
-        public Combiner(CombinerType type, Func<Node<TNode>, Node<TNode>, Node<TNode>> func)
+        public Combiner(CombinerType type, Func<Node<TNode>, Node<TNode>, Node<TNode>> func) : this(String.Empty, type, func) {}
+        /// <summary>
+        /// Create a new combiner.
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <param name="type">Compatible Node types</param>
+        /// <param name="func">Node combiner function</param>
+        public Combiner(string name, CombinerType type, Func<Node<TNode>, Node<TNode>, Node<TNode>> func)
         {
             this.func = func;
+            Name = name;
             Type = type;
         }
         /// <summary>
@@ -48,6 +57,12 @@ namespace APCSharp.Parser
         /// <param name="n2">Second Node</param>
         /// <returns>Node composed of two other Nodes</returns>
         public Node<TNode> Combine(Node<TNode> n1, Node<TNode> n2) => func(n1, n2);
+
+        public Combiner<TNode> NameBinder(string name)
+        {
+            Name = name;
+            return this;
+        }
 
         public static implicit operator Combiner(Combiner<TNode> n)
         {
@@ -63,26 +78,26 @@ namespace APCSharp.Parser
         public Combiner(CombinerType type, Func<Node<NodeType>, Node<NodeType>, Node<NodeType>> func) : base(type, func) { }
 
         /// <summary>
-        /// Preset combiner that concatinates the two Nodes values to a string with a custom NodeType typelabel.
+        /// Preset combiner that concatenates the two Nodes values to a string with a custom NodeType type-label.
         /// </summary>
         /// <param name="type">Result type</param>
         /// <returns>A new string combiner with a custom result type</returns>
         public static Combiner TypedString(NodeType type) => new Combiner((Node<NodeType> p1, Node<NodeType> p2) => new Node<NodeType>(type, p1.Value.ToString() + p2.Value.ToString()));
         /// <summary>
-        /// Preset combiner that concatinates the two Nodes values to a string.
+        /// Preset combiner that concatenates the two Nodes values to a string.
         /// </summary>
-        public static Combiner String = TypedString(NodeType.String);
+        public static Combiner String = TypedString(NodeType.String).NameBinder("String");
         /// <summary>
         /// Preset combiner that creates a branch Node from two other Nodes.
         /// </summary>
-        public static Combiner NodeList = new Combiner((Node<NodeType> p1, Node<NodeType> p2) => Node.List(p1, p2));
+        public static Combiner NodeList = new Combiner((Node<NodeType> p1, Node<NodeType> p2) => Node.List(p1, p2)).NameBinder("List");
         /// <summary>
         /// Discard the second Node.
         /// </summary>
-        public static Combiner First = new Combiner(CombinerType.Lists, (n1, n2) => n1);
+        public static Combiner First = new Combiner(CombinerType.Lists, (n1, n2) => n1).NameBinder("First");
         /// <summary>
         /// Discard the first Node.
         /// </summary>
-        public static Combiner Second = new Combiner(CombinerType.Lists, (n1, n2) => n2);
+        public static Combiner Second = new Combiner(CombinerType.Lists, (n1, n2) => n2).NameBinder("Second");
     }
 }
