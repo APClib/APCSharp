e4afcb50aea3666dbb93c5622576b7903046b9ee
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-03-22 09:25:03 +0100

    Added documentation

--- APCSharp/Util/Config.cs
+++ APCSharp/Util/Config.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace APCSharp.Util
 {
     /// <summary>
-    /// Configuration for the APC parser
+    /// Configuration for the APC parser.
     /// </summary>
     public static class Config
     {
@@ -14,9 +14,14 @@ namespace APCSharp.Util
         /// </summary>
         public static string Indentation = "    ";
         /// <summary>
-        /// Print debug info from each parser Function.
+        /// Print debug info from each parser.
         /// </summary>
+        /// <value>Enabled or disabled</value>
         public static bool Verbose = false;
+        /// <summary>
+        /// Dictionary of escaped special character mapped to their real values. Override with your own or provide it when using the Parser.CharBut parser combinator.
+        /// </summary>
+        /// <value>Map of special character mappings</value>
         public static Dictionary<string, char> StandardCharMapping = new Dictionary<string, char>
         {
             ["\\n"] = '\n'
